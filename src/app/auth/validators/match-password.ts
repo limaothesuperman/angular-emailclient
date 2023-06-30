43d6fd63c0863bd416567ce19e87e7314b431b25
Mao Li <mli94@stevens.edu>
2023-06-30 11:52:07 -0400

    added password validations

---
+++ src/app/auth/validators/match-password.ts
@@ -0,0 +1,16 @@
+import { AbstractControl, FormGroup, Validator } from "@angular/forms";
+import { Injectable } from "@angular/core";
+
+@Injectable({ providedIn: 'root' })
+export class MatchPassword implements Validator {
+    validate(formGroup: AbstractControl) {
+        const { password, passwordConfirmation } = formGroup.value;
+
+        if (password === passwordConfirmation) {
+            return null;
+        }
+        else {
+            return { passwordDontMatch: true };
+        }
+    }
+}
